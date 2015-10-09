0d9594186927319ad3c70ef0aeeab40aa86676b4
WarWeasle <warweasle1@gmail.com>
2015-10-09 18:01:39 +0000

    Made library.lisp look for the (unix only) library path.

--- library.lisp
+++ library.lisp
@@ -9,6 +9,14 @@
 
 ;; (cffi:define-foreign-library :libBullet
 ;;   (cffi-features:unix "/home/warweasle/quicklisp/local-projects/valentine/libbullet_wrap.so"))
+
+
+
+(cffi:load-foreign-library
+ 
+ (concatenate 'string
+	      (directory-namestring
+	       (asdf:system-relative-pathname :valentine "valentine.asd"))
+	      "libbullet_wrap.so"))
 				   
-(cffi:load-foreign-library "/home/warweasle/quicklisp/local-projects/valentine/libbullet_wrap.so")
 
